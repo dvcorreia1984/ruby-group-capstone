16a9fd2c312579e20f3549326f0f94585759ebd4
dvcorreia1984 <victor@onsleer.co.za>
2023-10-31 15:18:02 +0200

    Updated game.rb file

--- app.rb
+++ app.rb
@@ -9,33 +9,23 @@ class App
     @all_sources = nil
   end
 
-  def list_all_books
-  end
+  def list_all_books; end
 
-  def list_all_music_albums
-  end
+  def list_all_music_albums; end
 
-  def list_all_movies
-  end
+  def list_all_movies; end
 
-  def list_all_genres
-  end
+  def list_all_genres; end
 
-  def list_all_labels
-  end
+  def list_all_labels; end
 
-  def list_all_authors
-  end
+  def list_all_authors; end
 
-  def list_all_sources
-  end
+  def list_all_sources; end
 
-  def add_a_book
-  end
+  def add_a_book; end
 
-  def add_a_music_album
-  end
+  def add_a_music_album; end
 
-  def add_a_movie
-  end
+  def add_a_movie; end
 end
--- game.rb
+++ game.rb
@@ -1,3 +1,22 @@
-class Game
-    
-end 
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at, :author, :archived
+
+  def initialize(publish_date, multiplayer, last_played_at, _author, archived)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+    @author
+  end
+
+  private
+
+  def can_be_archived?
+    ten_years_ago = Date.today.prev_year(10)
+
+    Date.parse(@publish_date) < ten_years_ago
+
+    # add logic to check if the game has been played in the last 2 years
+    # if it has, it should not be archived
+    # if it hasn't, it should be archived
+  end
+end
