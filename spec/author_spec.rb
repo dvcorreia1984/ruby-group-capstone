e2860702d631612c7bbfd0975061921f575bda3c
dvcorreia1984 <victor@onsleer.co.za>
2023-11-03 18:10:49 +0200

    Added add_item test to author_spec.rb file

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../author'
+require_relative '../book'
 
 describe Author do
+
+  let (:author) { Author.new('John', 'Doe') }
+  let (:book) { Book.new('The Book', author, 'pink') }
+  let (:label) { Label.new('The Label', author) }
+
   it 'should have last_name' do
     author = Author.new('John', 'Doe')
     expect(author.last_name).to eq('Doe')
@@ -10,4 +16,11 @@ describe Author do
     author = Author.new('John', 'Doe')
     expect(author.first_name).to eq('John')
   end
+
+  it 'should associate and add an item to the author' do
+    author.add_item(book)
+    expect(book.author).to eq(author)
+    expect(author.items).to include(book)
+  end
+
 end
